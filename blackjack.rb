d739d54877882d8a4941f4906ba52e308f80062f
renjuzac <renjuzac@gmail.com>
2015-10-24 10:27:23 -0700

    Refactored for single score instead of combinations

--- blackjack.rb
+++ blackjack.rb
@@ -17,7 +17,7 @@ end
 def card_value(card)
   case card[0]
   when "Ace"
-    value = 1
+    value = 11
   when "King"
     value = 10
   when "Queen"
@@ -35,77 +35,53 @@ def deal_card(deck)
   selected_card = deck.delete(pick)
 end
 
-def calculate_score(hand)
-  score_combinations = [[]]
-  new_score_combinations = []
-  hand.each do |card|
-    card_val = card_value([card[0]])
-    if card[0] == "Ace"
-      new_score_combinations = []
-      score_combinations.each do |value| 
-        new_score_combinations << value + ([card_val])
-        new_score_combinations << value + ([card_val+10])
-      end
-      score_combinations = new_score_combinations
-    else
-        new_score_combinations = []
-        score_combinations.each do |value| 
-        new_score_combinations << value + ([card_val])
-        end
-        score_combinations = new_score_combinations
-    end
+def calculate_score(hand)      # Returns current highest valid  score
+  score = 0
+  ace_count = hand.select {|card| card[0] == "Ace"}.count
+  hand.each do |card|  
+    score += card_value(card)
   end
-
-  scores = score_combinations.map{|score| score.inject(0) {|sum,val| sum+val} }
-  scores.uniq
+  while ace_count > 0 && score > 21 do 
+    score -= 10
+    ace_count -= 1
+  end
+  score
 end
 
-def check_winner(player_score,dealer_score)
-  player_score_max = (player_score.select {|score| score <=21}).max
-  dealer_score_max = (dealer_score.select {|score| score <=21}).max
-  
-  if player_score.include?(21) 
+def check_winner(player_score,dealer_score)  
+  if player_score == 21
     return "\nPlayer wins !! BlackJack"
-  elsif dealer_score.include?(21)
+  elsif dealer_score ==21 
     return "\nDealer wins !! BlackJack"
-  elsif !player_score_max 
+  elsif player_score > 21
     return "\nPlayer Busted , Dealer Wins"
-  elsif !dealer_score_max
+  elsif dealer_score > 21
     return "\nDealer Busted , Player Wins !!"         
-  elsif player_score_max > dealer_score_max
+  elsif player_score  > dealer_score
     return "\nPlayer Wins"
-  elsif dealer_score_max > player_score_max 
+  elsif dealer_score  > player_score 
     return "\nDealer Wins"
   else
     return "Its a Tie"
   end
 end
 
-def valid_player_score(player_score)
-  valid_scores = player_score.select { |val| val <21}
-  !valid_scores.empty?
-end
-
-def valid_dealer_score(dealer_score)
-  valid_scores = dealer_score.select { |val| val < 17}
-  !valid_scores.empty?
+def valid_score(score,max)
+  score < max
 end
 
 def display_hand_and_score(dealer_hand,dealer_score,player_hand,player_score)
   puts "\n \n"
   puts "Dealer hand #{dealer_hand}"
-  puts "Dealer Score : #{valid_dealer_score(dealer_score) ? dealer_score.select {|score| score <=21} : dealer_score.min }"
+  puts "Dealer Score : #{dealer_score}"
   puts "Player hand #{player_hand}"
-  puts "Player Score : #{ valid_player_score(player_score) ? player_score.select {|score| score <=21} : player_score.min}"
+  puts "Player Score : #{player_score}"
 end
 
 playing_deck = initialize_deck
 
-
 player_hand = []
 dealer_hand = []
-player_score = [] 
-dealer_score = []
 
 player_hand << deal_card(playing_deck)
 dealer_hand << deal_card(playing_deck)
@@ -115,7 +91,7 @@ player_score = calculate_score(player_hand)
 
 puts "\n\n  Welcome to BlackJack !!  \n\n"
 
-if player_score.include?(21) or dealer_score.include?(21)
+if player_score == 21 or dealer_score == 21
   display_hand_and_score(dealer_hand,dealer_score,player_hand,player_score)
   puts "#{check_winner(player_score,dealer_score)}"
   exit
@@ -126,24 +102,24 @@ begin
   puts "Dealer hand #{dealer_hand}"
   puts "Player hand #{player_hand}"
   player_score = calculate_score(player_hand)
-  puts "Player Score : #{ valid_player_score(player_score) ? player_score.select {|score| score <=21} : player_score.min}"
-  break if !valid_player_score(player_score)      
+  puts "Player Score : #{player_score}"
+  break if !valid_score(player_score,21)      
   begin
-    puts "Hit or Pass (H/P)"
+    puts "Hit or Stay (H/S)"
     user_pick = gets.chomp.upcase
-  end while !["H","P"].include?(user_pick)
-end while (user_pick != "P" && valid_player_score(player_score)) 
+  end while !["H","S"].include?(user_pick)
+end while (user_pick != "S" && valid_score(player_score,21)) 
 
-if player_score.include?(21) or player_score.select {|score| score <=21}.empty?
+if player_score == 21
   display_hand_and_score(dealer_hand,dealer_score,player_hand,player_score)
   puts "#{check_winner(player_score,dealer_score)}"
   exit
 else 
-  while valid_dealer_score(dealer_score) do
+  while valid_score(dealer_score,17) do
     dealer_hand << deal_card(playing_deck)
     puts "Dealer hand #{dealer_hand}"
     dealer_score = calculate_score(dealer_hand)
-    puts "Dealer Score : #{valid_dealer_score(dealer_score) ? dealer_score.select {|score| score <=21} : dealer_score.min }"
+    puts "Dealer Score : #{dealer_score }"
   end 
   display_hand_and_score(dealer_hand,dealer_score,player_hand,player_score)
   puts "#{check_winner(player_score,dealer_score)}"
